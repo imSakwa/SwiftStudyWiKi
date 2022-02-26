73c30441dae43d11544e60a0f816d8719d53b37e
ChangMin <sopg04@gmail.com>
2022-02-26 23:14:04 +0900

    Initial Commit

---
+++ SwiftStudyWiKi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftStudyWiKi
+//
+//  Created by ChangMin on 2022/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
