9228df585f2b9f01750a22dc7370bfc84d56f5c4
ChangMin <sopg04@gmail.com>
2022-02-27 22:16:12 +0900

    커스텀 네비게이션 컨트롤러 추가

--- SwiftStudyWiKi/MyNavigationController.swift
+++ SwiftStudyWiKi/MyNavigationController.swift
@@ -13,7 +13,19 @@ class MyNavigationController: UINavigationController {
         super.viewDidLoad()
         
 
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithDefaultBackground()
         
+        appearance.backgroundColor = .green
+        appearance.shadowColor = .clear
+        appearance.titleTextAttributes =  [NSAttributedString.Key.foregroundColor: UIColor.brown]
+          
+        self.navigationController?.navigationBar.tintColor = .yellow
+        self.navigationController?.navigationBar.standardAppearance = appearance
+        self.navigationController?.navigationBar.scrollEdgeAppearance = self.navigationController?.navigationBar.standardAppearance
+        self.navigationBar.setNeedsLayout()
+        
+        self.navigationBar.frame.size = self.navigationBar.sizeThatFits(CGSize(width: self.navigationBar.frame.size.width, height: 50))
     }
     
     func setNaviBarColor() {
@@ -27,5 +39,18 @@ class MyNavigationController: UINavigationController {
         self.navigationController?.navigationBar.tintColor = .yellow
         self.navigationController?.navigationBar.standardAppearance = appearance
         self.navigationController?.navigationBar.scrollEdgeAppearance = self.navigationController?.navigationBar.standardAppearance
+    }
+}
+
+extension UINavigationBar {
+    override open func sizeThatFits(_ size: CGSize) -> CGSize {
+        var barHeight: CGFloat = 50
+    
+        if size.height == 50 {
+            barHeight = size.height
+        }
+    
+        let newSize: CGSize = CGSize(width: self.frame.size.width, height: barHeight)
+        return newSize
     }
 }
