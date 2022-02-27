b40b3267bc615ea5b24e88912dfb85df1e2ae943
ChangMin <sopg04@gmail.com>
2022-02-27 22:16:01 +0900

    테이블 뷰 추가

--- SwiftStudyWiKi/MainViewController.swift
+++ SwiftStudyWiKi/MainViewController.swift
@@ -6,21 +6,63 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class MainViewController: UIViewController {
 
+    let subjectTableView = UITableView().then {
+        $0.backgroundColor = .red
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
+        self.view.backgroundColor = .white
         
-        self.navigationItem.title = "hi"
-        
-        if let navi = self.navigationController as? MyNavigationController {
-            navi.setNaviBarColor()
-        }
-        // Do any additional setup after loading the view.
+        self.navigationItem.title = "Chagmn's Swift WiKi📕"
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    private func setSubjectTableView() {
+        subjectTableView.dataSource = self
+        subjectTableView.delegate = self
+        subjectTableView.register(SubjectTableCell.self, forCellReuseIdentifier: "SubjectCell")
+    }
+    
+    private func setLayout() {
+        self.view.addSubview(subjectTableView)
+        subjectTableView.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
 }
 
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "SubjectCell", for: indexPath) as? SubjectTableCell else {
+            return UITableViewCell()
+        }
+        
+        cell.backgroundColor = .green
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
---
+++ SwiftStudyWiKi/SubjectTableCell.swift
@@ -0,0 +1,22 @@
+//
+//  SubjectTableCell.swift
+//  SwiftStudyWiKi
+//
+//  Created by ChangMin on 2022/02/27.
+//
+
+import UIKit
+
+class SubjectTableCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+    
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
