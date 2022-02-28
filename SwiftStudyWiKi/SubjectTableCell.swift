fd9bcbe0f79c7d92e11788db45895b26cb6bda88
ChangMin <sopg04@gmail.com>
2022-02-28 23:27:02 +0900

    셀 테스트

--- SwiftStudyWiKi/SubjectTableCell.swift
+++ SwiftStudyWiKi/SubjectTableCell.swift
@@ -6,13 +6,39 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class SubjectTableCell: UITableViewCell {
+    
+    let titleLbl = UILabel().then {
+        $0.font = .systemFont(ofSize: 17)
+    }
+    
+    let nextIcon = UIImageView().then {
+        $0.image = UIImage(systemName: "chevron.right")
+    }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        setLayout()
+    }
     
+    private func setLayout() {
+        
+        self.addSubview(titleLbl)
+        titleLbl.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(10)
+        }
+        
+        self.addSubview(nextIcon)
+        nextIcon.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(10)
+            
+        }
     }
     
     required init?(coder: NSCoder) {
