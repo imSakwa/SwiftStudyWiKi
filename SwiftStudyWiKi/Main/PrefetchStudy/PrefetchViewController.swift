7609b5c6294b509d48aecd2538d67a1d01cf1dcd
ChangMin <sopg04@gmail.com>
2022-03-01 19:01:45 +0900

    prefetchVC 생성

---
+++ SwiftStudyWiKi/Main/PrefetchStudy/PrefetchViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PrefetchViewController.swift
+//  SwiftStudyWiKi
+//
+//  Created by ChangMin on 2022/03/01.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class PrefetchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        
+        self.navigationItem.title = "Prefetch"
+        
+        
+        
+    }
+}
