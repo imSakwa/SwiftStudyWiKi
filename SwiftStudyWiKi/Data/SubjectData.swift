8b9ea6428dc5f0c7116971d852942117309abe17
ChangMin <sopg04@gmail.com>
2022-03-07 22:22:47 +0900

    마지막 테스트 커밋..

---
+++ SwiftStudyWiKi/Data/SubjectData.swift
@@ -0,0 +1,14 @@
+//
+//  SubjectData.swift
+//  SwiftStudyWiKi
+//
+//  Created by ChangMin on 2022/03/01.
+//
+
+import Foundation
+
+struct Subject: Decodable {
+    var titles: [String] = [
+        "Prefetch 연습하기"
+    ]
+}
