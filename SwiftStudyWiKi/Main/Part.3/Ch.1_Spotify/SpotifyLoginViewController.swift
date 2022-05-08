a76dfe12163ef75ead31067ecb5d625f7e293342
ChangMin <sopg04@gmail.com>
2022-05-08 22:49:31 +0900

    Part 3 -Ch.1 스포티파이 로그인 화면 샘플 - 애플로그인

--- SwiftStudyWiKi/Main/Part.3/Ch.1_Spotify/SpotifyLoginViewController.swift
+++ SwiftStudyWiKi/Main/Part.3/Ch.1_Spotify/SpotifyLoginViewController.swift
@@ -10,8 +10,17 @@ import SnapKit
 import Then
 import GoogleSignIn
 import Firebase
+import FirebaseAuth
+import AuthenticationServices
+import CryptoKit
 
-class SpotifyLoginViewController: UIViewController {
+class SpotifyLoginViewController: UIViewController, ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        <#code#>
+    }
+    
+    
+    private var currentNonce: String?
     
     let loginStack = UIStackView().then {
         $0.axis = .vertical
@@ -91,8 +100,9 @@ class SpotifyLoginViewController: UIViewController {
                 mainVC.loginType = .google
                 self.navigationController?.pushViewController(mainVC, animated: true)
             }
-        } else {
             
+        } else {
+            startSignInWithAppleFlow()
         }
     }
 
@@ -141,6 +151,97 @@ class SpotifyLoginViewController: UIViewController {
         appleLoginBtn.snp.makeConstraints {
             $0.height.equalTo(60)
             $0.width.equalToSuperview()
+        }
+    }
+}
+
+extension SpotifyLoginViewController {
+    func startSignInWithAppleFlow() {
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashStr = hashedData.compactMap {
+            return String(format: "02x", $0)
+        }.joined()
+        
+        return hashStr
+    }
+    
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: Array<Character> = Array("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+        
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0..<16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                
+                if errorCode != errSecSuccess {
+                    fatalError("에러 \(errorCode)")
+                }
+                return random
+            }
+            
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        return result
+    }
+}
+
+extension SpotifyLoginViewController: ASAuthorizationControllerDelegate {
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            
+            guard let nonce = currentNonce else {
+                
+            }
+            
+            guard let appleIDToken = appleIDCredential.identityToken else {
+                fatalError("에러")
+                return
+            }
+            
+            guard let idTokenStr = String(data: appleIDToken, encoding: .utf8) else {
+                fatalError("에러")
+                return
+            }
+            
+            let credential = OAuthProvider.credential(withProviderID: "apple.com", idToken: idTokenStr, rawNonce: nonce)
+            
+            Auth.auth().signIn(with: credential) { authResult, error in
+                if let error = error {
+                    print("에러")
+                    return
+                }
+                
+                let mainVC = SpotifyMainViewController()
+                mainVC.loginType = .apple
+                self.navigationController?.pushViewController(mainVC, animated: true)
+            }
         }
     }
 }
