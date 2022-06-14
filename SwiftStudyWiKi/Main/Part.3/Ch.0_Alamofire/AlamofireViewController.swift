dc2610b59a0402948dddda81599d228b8563bb35
ChangMin <sopg04@gmail.com>
2022-06-14 23:42:07 +0900

    테스트

--- SwiftStudyWiKi/Main/Part.3/Ch.0_Alamofire/AlamofireViewController.swift
+++ SwiftStudyWiKi/Main/Part.3/Ch.0_Alamofire/AlamofireViewController.swift
@@ -11,5 +11,13 @@ import Then
 import Alamofire
 
 class AlamofireViewController: UIViewController {
+    private let url = "https://api.corona-19.kr/korea/country/new/"
+    private let param = [
+        "serviceKey": "AUS6MojRdvVFi9WZIpucnEGz4eChKys8g"
+    ]
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
 }
